b981f246f384f724e5064be79413f2971470a871
charliehuang09 <charliehuang09@gmail.com>
2022-12-16 11:27:44 -0800

    Finshed

--- main.cpp
+++ main.cpp
@@ -3,10 +3,10 @@
 #include <cmath>
 using namespace std;
 
-struct derivatives{
+struct Derivatives{
     vector<vector<vector<float>>> weights;
     vector<vector<float>> bias;
-    derivatives(vector<vector<vector<float>>> weights_, vector<vector<float>> bias_){
+    Derivatives(vector<vector<vector<float>>> weights_, vector<vector<float>> bias_){
         weights = weights_;
         bias = bias_;
     };
@@ -19,13 +19,6 @@ class NN {
     float relu(float x){
         return max(0.0f,x);
     };
-    float derivative_relu(float x){
-        return 1;
-        return  x < 1;
-    }
-    float sigmoid(float x){
-        return 1/(1+exp(x));
-    }
     float dot(vector<float> inputs, vector<float> weights){
         float activation = 0;
         for (float i = 0; i < weights.size(); i++) activation += inputs[i] * weights[i];
@@ -34,7 +27,7 @@ class NN {
     float cost(float a, float b){
         return (a-b)*(a-b);
     }
-    float getDerivative(int prevDerivative, int weight){
+    float getDerivative(float prevDerivative, float weight){
         return relu(prevDerivative * weight);
     }
     vector<float> add(vector<vector<float>> input){
@@ -50,20 +43,13 @@ class NN {
         return output;
     }
     public:
-    void floatialize(float inputs, float outputs, float w, float b){//3 2
+    void initialize(float inputs, float outputs, float w, float b){//3 2
         vector<vector<float>> newLayer;
         vector<float> curr;
         vector<float> t;
-        for (float i = 0; i < outputs; i++) t.push_back(b);
-//        for (float i = 0; i < inputs; i++){
-//            curr.clear();
-//            for (float j = 0; j < outputs; j++){
-//                curr.push_back(w);
-//            }
-//            newLayer.push_back(curr);
-//        }
-        for (float i = 0; i < inputs; i++) curr.push_back(w);
-        for (float j = 0; j < outputs; j++) newLayer.push_back(curr);
+        for (int i = 0; i < outputs; i++) t.push_back(b);
+        for (int i = 0; i < inputs; i++) curr.push_back(w);
+        for (int j = 0; j < outputs; j++) newLayer.push_back(curr);
         weights.push_back(newLayer);
         bias.push_back(t);
     };
@@ -71,9 +57,9 @@ class NN {
         vector<vector<float>> newLayer;
         vector<float> connections;
         vector<float> t;
-        for (float i = 0; i < size; i++) t.push_back(b);
-        for (float i = 0; i < weights[weights.size() - 1].size(); i++) connections.push_back(w);
-        for (float i = 0; i < size; i++) newLayer.push_back(connections);
+        for (int i = 0; i < size; i++) t.push_back(b);
+        for (int i = 0; i < weights[weights.size() - 1].size(); i++) connections.push_back(w);
+        for (int i = 0; i < size; i++) newLayer.push_back(connections);
         weights.push_back(newLayer);
         bias.push_back(t);
     };
@@ -81,29 +67,30 @@ class NN {
         vector<float> prev;//activation of prev layer
         vector<float> curr;
         prev.push_back(input);
-        for (float i = 0; i < weights.size(); i++){
+        for (int i = 0; i < weights.size(); i++){
             curr.clear();
-            for (float j = 0; j < weights[i].size(); j++){
+            for (int j = 0; j < weights[i].size(); j++){
                 curr.push_back(relu(dot(prev, weights[i][j])) + bias[i][j]);
             }
             prev = curr;
         }
         return prev[0];
     }
-    vector<vector<vector<float>>> backProp(float X, float Y){
+    Derivatives backProp(float X, float Y){
         vector<vector<vector<float>>> weightDerivatives;
         vector<vector<float>> layerDerivative;
         vector<vector<float>> biasDerivatives;
         vector<float> prevDerivative;
         vector<float> currDerivative;
         prevDerivative.push_back(predict(X) - Y);
-        for (float i = weights.size() - 1; i >= 0; i--){
+        biasDerivatives.push_back(prevDerivative);
+        for (int i = weights.size() - 1; i >= 0; i--){
             layerDerivative.clear();
-            for (float j = 0; j < weights[i].size(); j++){
+            for (int j = 0; j < weights[i].size(); j++){
                 currDerivative.clear();
                 for (int k = 0; k < weights[i][j].size(); k++){
 //                    currDerivative.push_back(weights[i][j][k]*relu(prevDerivative[j]));
-                    currDerivative.push_back(getDerivative(weights[i][j][k], prevDerivative[j]));
+                    currDerivative.push_back(getDerivative(prevDerivative[j], weights[i][j][k]));
                 }
                 layerDerivative.push_back(currDerivative);
             }
@@ -111,18 +98,39 @@ class NN {
             biasDerivatives.push_back(prevDerivative);
             weightDerivatives.push_back(layerDerivative);
         }
-        return weightDerivatives;
+        reverse(weightDerivatives.begin(), weightDerivatives.end());
+        reverse(biasDerivatives.begin(), biasDerivatives.end());
+        biasDerivatives.pop_back();
+        return Derivatives(weightDerivatives, biasDerivatives);
+    };
+    void fit(float X, float Y, float lr, int epochs){
+        for (int epoch = 0; epoch < epochs; epoch++){
+            Derivatives derivatives = backProp(X, Y);
+            for (int i = 0; i < weights.size(); i++){
+                for (int j = 0; j < weights[i].size(); j++){
+                    for (int k = 0; k < weights[i][j].size(); k++){
+                        weights[i][j][k] -= lr*derivatives.weights[i][j][k];
+                    }
+                }
+            }
+            for (int i = 0; i < bias.size(); i++){
+                for (int j = 0; j < bias[i].size(); j++){
+                    bias[i][j] -= lr*derivatives.bias[i][j];
+                }
+            }
+        }
     }
 };
 
 int main(){
     //predict test
     NN model;
-    model.floatialize(1, 3, 2, 1);
-    model.create_layer(3, 1, -2);
-    model.create_layer(3, 1, -2);
-    cout << model.predict(1);
+    model.initialize(1, 3, 2, 1);
+    model.create_layer(1, 1, -2);
+    cout << model.predict(1) << " ";
+    model.fit(1.0, 1.0, 0.01, 1000);
     model.backProp(1.0, 1.0);
+    cout << model.predict(1.0);
     return 0;
 }
 //  3
